6956d8e7de12045aed5036010ed84746ff6946e3
Weslley Cardoso <weslleycardoso011@gmail.com>
2025-10-04 15:07:55 -0300

    Teste, aplicaçao esta no ar

---
+++ src/main/java/br/com/weslley/Buscador/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.weslley.Buscador.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Anotação que diz ao Spring: "Esta classe é um Controller de API"
+public class HomeController {
+
+    @GetMapping("/") // Anotação que diz: "Quando alguém acessar a URL raiz ('/'), execute este método"
+    public String home() {
+        return "Minha primeira API está no ar!";
+    }
+}
